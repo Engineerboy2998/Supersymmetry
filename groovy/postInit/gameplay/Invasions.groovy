1a1575160cb82ca4b8872927f308a1218530a39f
oliwier509 <53867161+oliwier509@users.noreply.github.com>
2025-03-21 23:14:37 +0100

    Invasions.groovy now include the mortar clusters
    title is self explanatory

--- groovy/postInit/gameplay/Invasions.groovy
+++ groovy/postInit/gameplay/Invasions.groovy
@@ -2,7 +2,12 @@ import supersymmetry.api.event.MobHordeEvent;
 import net.minecraft.entity.monster.EntityZombie;
 import net.minecraft.init.MobEffects;
 import net.minecraft.potion.PotionEffect;
+import net.minecraft.util.ResourceLocation;
+import net.minecraft.nbt.JsonToNBT;
+import net.minecraft.nbt.NBTException;
+import net.minecraft.nbt.NBTTagCompound;
 import techguns.entities.npcs.Bandit;
+import net.minecraft.entity.item.EntityFallingBlock;
 
 new MobHordeEvent((player) -> {EntityZombie zombie = new EntityZombie(player.world);
 			        zombie.addPotionEffect(new PotionEffect(MobEffects.SPEED, 999999, 1));
@@ -26,3 +31,8 @@ new MobHordeEvent((player) -> {Bandit bandit = new Bandit(player.world); bandit.
 	.setAdvancementUnlock(new ResourceLocation("gregtech:low_voltage/23_lv_assembler"))
 	.setTimer(72000, 216000)	        // 1 - 3 hours
 
+
+String ScriptNBT = "{Block:\"reccomplex:spawn_script\",Time:1,TileEntityData:{redstoneTriggerable:1b,spawnTriggerable:1b,script:{scripts:[{id:\"command\",script:{commands:[{command:\"kill @e[type=susy:drop_pod,c=1,r=2]\",weight:1.0d}]}},{id:\"command\",script:{commands:[{command:\"/setblock ~ ~1 ~ air\",weight:1.0d}]}},{id:\"strucGen\",script:{structureListID:\"Mortar_Cluster\",structureShift_y:0,structureShift_x:0,structures:[],simpleMode:0b,structureShift_z:0}}]}}}";
+
+new MobHordeEvent((player) -> {}, 10, 10, "mortar_cluster")
+    .beScriptBlock(ScriptNBT)
